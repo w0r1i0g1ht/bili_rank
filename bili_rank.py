92d525cffcf257cf322c7f3b57177508f901952a
丘丘人之大都督 <78846391+Wright-666@users.noreply.github.com>
2022-08-12 17:36:47 +0800

    Add files via upload

---
+++ bili_rank.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+import requests
+import re
+
+def main():
+    url = 'https://www.bilibili.com/v/popular/rank/all'
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0"}
+    page = requests.get(url=url,headers=header)
+    page = page.text
+    obj = re.compile(r'<li data-id=.*?data-rank="(?P<rank>.*?)".*?<a href="(?P<url>.*?)".*?class="title">(?P<title>.*?)</a>'
+                     r'.*?alt="up">(?P<up>.*?)</span>.*?alt="play">(?P<play>.*?)</span>.*?alt="like">(?P<like>.*?)</span>',re.S)
+    result = obj.finditer(page)
+    for it in result:
+        print("排名",it.group("rank"))
+        print("网址","https:"+it.group("url"))
+        print("标题",it.group("title"))
+        print("up主",it.group("up").strip())
+        print("播放量",it.group("play").strip())
+        print("弹幕",it.group("like").strip())
+        print("\n")
+    pass
+
+
+if __name__ == '__main__':
+    main()
