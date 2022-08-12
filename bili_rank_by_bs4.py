0fb117209836af5e5273d653ba3f8b2d7d6bf49e
丘丘人之大都督 <78846391+Wright-666@users.noreply.github.com>
2022-08-13 00:01:30 +0800

    Add files via upload

---
+++ bili_rank_by_bs4.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    url = "https://www.bilibili.com/v/popular/rank/all"
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0",
+              "Accept": "application / json, text / plain, * / *",
+              "Accept - Encoding": "gzip, deflate, br",
+              "Connection": "keep - alive"
+              }
+    page = requests.get(url=url, headers=header)
+    page = BeautifulSoup(page.text,"html.parser")
+    lis = page.find_all("li",attrs={"class":"rank-item"})
+    for li in lis:
+        rank = li.get("data-rank")
+        address = li.find("div",attrs={"class":"info"}).find("a").get("href")
+        title = li.find("div",attrs={"class":"info"}).find("a").text
+        up = li.find("div",attrs={"class":"detail"}).find("span").text
+        play = li.find("div",attrs={"class":"detail-state"}).find_all("span")[0].text
+        like = li.find("div", attrs={"class": "detail-state"}).find_all("span")[1].text
+        print("排名",rank)
+        print("网址", "https:" + address)
+        print("标题",title)
+        print("up主",up.strip())
+        print("播放量",play.strip())
+        print("弹幕",like.strip())
+        print("")
+    # print(lis)
+    pass
+
+
+if __name__ == '__main__':
+    main()
