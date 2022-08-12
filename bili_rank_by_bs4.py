007badbf305b7f351a2f77854f04f6909b68051a
丘丘人之大都督 <78846391+Wright-666@users.noreply.github.com>
2022-08-13 00:09:13 +0800

    bs4

--- bili_rank_by_bs4.py
+++ bili_rank_by_bs4.py
@@ -11,7 +11,7 @@ def main():
               "Connection": "keep - alive"
               }
     page = requests.get(url=url, headers=header)
-    page.encoding="utf-8"
+    page.encoding = "utf-8"
     page = BeautifulSoup(page.text,"html.parser")
     lis = page.find_all("li",attrs={"class":"rank-item"})
     for li in lis:
